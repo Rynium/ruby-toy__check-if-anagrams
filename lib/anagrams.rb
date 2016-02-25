b157d82af4a745631649c31946b895b8d6b23898
rynium <ryanstantz@gmail.com>
2016-02-25 01:44:48 +0000

    Done

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,24 @@
-def anagrams?(str1, str2)
+def inventory(string)
+    empty = {}
+    
+    i = 0 
+    while i < string.length
+        if empty.has_key?(string[i].to_sym)
+            empty[string[i].to_sym] += 1
+        else 
+            empty[string[i].to_sym] = 1
+        end
+    i += 1
+    end
+    empty
 end
+
+def anagrams?(str1, str2)
+    outcome = false
+    
+    if inventory(str1) == inventory(str2)
+         outcome = true
+    end
+    outcome
+end
+
